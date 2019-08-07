cd1d4d8444027790f2283733b11254749004c088
Sabrina Gannon <sabrinagannon@icloud.com>
2019-08-07 19:16:26 -0400

    Initial

---
+++ colors.js
@@ -0,0 +1,18 @@
+// This file can be used with the themer CLI, see https://github.com/mjswensen/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#6261A1",
+    "shade7": "#FFC9C9",
+    "accent0": "#820000",
+    "accent1": "#FFFFFF",
+    "accent2": "#FFEBC4",
+    "accent3": "#00A7A7",
+    "accent4": "#3898FF",
+    "accent5": "#B5E5E5",
+    "accent6": "#000000",
+    "accent7": "#8BC9F0"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%236261A1&colors.dark.shade7=%23FFC9C9&colors.dark.accent0=%23820000&colors.dark.accent1=%23FFFFFF&colors.dark.accent4=%233898FF&colors.dark.accent2=%23FFEBC4&colors.dark.accent3=%2300A7A7&colors.dark.accent5=%23B5E5E5&colors.dark.accent6=%23000000&colors.dark.accent7=%238BC9F0&colors.light.shade0=&colors.light.shade7=&colors.light.accent0=&colors.light.accent1=&colors.light.accent2=&activeColorSet=dark
